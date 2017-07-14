171a66e1b62405ae768fa122c3cdcf2dfa4488ec
Scott <Gavin.Scott@pharmerica.com>
2017-07-14 13:12:17 -0400

    favicon to root

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -35,7 +35,7 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+  console.log('[ServiceWorker] Install', e);
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
@@ -45,7 +45,7 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+  console.log('[ServiceWorker] Activate', e);
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
